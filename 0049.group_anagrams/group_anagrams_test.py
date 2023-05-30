dd0a4b07d5a92de5a6626824b3779dc68ce3f9e0
Meeval Das <meevadas13@gmail.com>
2023-05-30 20:23:12 +0530

    feat(0049): optimised solution

--- 0049.group_anagrams/group_anagrams_test.py
+++ 0049.group_anagrams/group_anagrams_test.py
@@ -1,14 +1,26 @@
 import unittest
+from operator import itemgetter
+from typing import List
 from group_anagrams import Solution
 
 
 class TestGroupAnagrams(unittest.TestCase):
-    def test_group_anagrams_example_one(self):
-        self.assertAlmostEqual(Solution().group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]),
-                               [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]])
+    def sort_list_of_list(self, list_of_list: List[List[str]]):
+        for lst in list_of_list:
+            lst.sort()
+
+        list_of_list.sort(key=itemgetter(0))
+        return list_of_list
+
+
+    def test_one(self):
+        self.assertListEqual(self.sort_list_of_list(Solution().group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])),
+                             self.sort_list_of_list([["bat"], ["nat", "tan"], ["eat", "tea", "ate"]]))
+    # def test_group_anagrams_example_one(self):
+    #     self.assertAlmostEqual(sorted(Solution().group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])), sorted([["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]))
 
     def test_group_anagrams_example_two(self):
-        self.assertAlmostEqual(Solution().group_anagrams([""]), [[""]])
+        self.assertListEqual(self.sort_list_of_list(Solution().group_anagrams([""])), self.sort_list_of_list([[""]]))
 
     def test_group_anagrams_example_three(self):
-        self.assertAlmostEqual(Solution().group_anagrams(["a"]), [["a"]])
+        self.assertListEqual(self.sort_list_of_list(Solution().group_anagrams(["a"])), self.sort_list_of_list([["a"]]))
