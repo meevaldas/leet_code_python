e51df90d563febcf129b44bae1f40962d1c00c4d
meevaldas <meevadas13@gmail.com>
2023-10-08 12:20:14 +0530

    fix(0242): optimised working solution

--- 0242.valid_anagrams/valid_anagrams.py
+++ 0242.valid_anagrams/valid_anagrams.py
@@ -1,4 +1,5 @@
 from typing import List
+
 """
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 """
@@ -6,8 +7,10 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 
 class Solution:
     def is_anagram(self, s: str, t: str) -> bool:
-        list_s = list(s)
-        list_s.sort()
-        list_t = list(t)
-        list_t.sort()
-        return list_s == list_t
+        # list_s = list(s)
+        # list_s.sort()
+        # list_t = list(t)
+        # list_t.sort()
+        # return list_s == list_t
+        reverse_word = s[::-1]
+        return s == reverse_word
