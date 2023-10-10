8255c62ec2447f0f7a300a3e5b47ef1167f62bbe
meevaldas <meevadas13@gmail.com>
2023-10-10 21:58:51 +0530

    chore(0049): bad solution

--- 0049.group_anagrams/group_anagrams.py
+++ 0049.group_anagrams/group_anagrams.py
@@ -3,22 +3,33 @@ Given an array of strings strs, group the anagrams together. You can return the 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
 typically using all the original letters exactly once.
 """
-
+import collections
 from typing import List
 
 
 class Solution:
     def group_anagrams(self, strs: List[str]) -> List[List[str]]:
+        # dictionary = {}
+        # for word in strs:
+        #     sorted_word = ''.join(sorted(word))
+        #     # If word is not in dictionary
+        #     if sorted_word not in dictionary:
+        #         dictionary[sorted_word] = [word]
+        #     # If previously it is present that means its anagram
+        #     # was previously present
+        #     else:
+        #         dictionary[sorted_word] += [word]
+        # return [dictionary[i] for i in dictionary]
+        # dic = collections.defaultdict(list)
+        # for word in strs:
+        #     dic[''.join(sorted(word))].append(word)
+        # return dic.values()
+        ans = collections.defaultdict(list)
 
-        dictionary = {}
-        for word in strs:
-            sorted_word = ''.join(sorted(word))
-            # If word is not in dictionary
-            if sorted_word not in dictionary:
-                dictionary[sorted_word] = [word]
-            # If previously it is present that means its anagram
-            # was previously present
-            else:
-                dictionary[sorted_word] += [word]
-        return [dictionary[i] for i in dictionary]
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return sorted(list(ans.values()))
 
--- 0049.group_anagrams/group_anagrams_test.py
+++ 0049.group_anagrams/group_anagrams_test.py
@@ -5,20 +5,25 @@ from group_anagrams import Solution
 
 
 class TestGroupAnagrams(unittest.TestCase):
-    def sort_list_of_list(self, list_of_list: List[List[str]]):
-        for lst in list_of_list:
-            lst.sort()
+    def test_example_one(self):
+        self.assertCountEqual(Solution().group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]),[["bat"], ["nat", "tan"], ["ate", "eat", "tea"]])
 
-        list_of_list.sort(key=itemgetter(0))
-        return list_of_list
+    # def sort_list_of_list(self, list_of_list: List[List[str]]):
+    #     for lst in list_of_list:
+    #         lst.sort()
+    #
+    #     list_of_list.sort(key=itemgetter(0))
+    #     return list_of_list
+    #
+    # def test_one(self):
+    #     self.assertListEqual(
+    #         self.sort_list_of_list(Solution().group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])),
+    #         self.sort_list_of_list([["bat"], ["nat", "tan"], ["eat", "tea", "ate"]]))
+    #
+    # def test_group_anagrams_example_two(self):
+    #     self.assertListEqual(self.sort_list_of_list(Solution().group_anagrams([""])), self.sort_list_of_list([[""]]))
+    #
+    # def test_group_anagrams_example_three(self):
+    #     self.assertListEqual(self.sort_list_of_list(Solution().group_anagrams(["a"])), self.sort_list_of_list([["a"]]))
 
-    def test_one(self):
-        self.assertListEqual(
-            self.sort_list_of_list(Solution().group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])),
-            self.sort_list_of_list([["bat"], ["nat", "tan"], ["eat", "tea", "ate"]]))
 
-    def test_group_anagrams_example_two(self):
-        self.assertListEqual(self.sort_list_of_list(Solution().group_anagrams([""])), self.sort_list_of_list([[""]]))
-
-    def test_group_anagrams_example_three(self):
-        self.assertListEqual(self.sort_list_of_list(Solution().group_anagrams(["a"])), self.sort_list_of_list([["a"]]))
