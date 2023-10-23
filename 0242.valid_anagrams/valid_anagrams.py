c3879a417d7dd1e58933b08441246eafef322df8
meevaldas <meevadas13@gmail.com>
2023-10-23 10:14:45 +0530

    fix(0242): optimized solution

--- 0242.valid_anagrams/valid_anagrams.py
+++ 0242.valid_anagrams/valid_anagrams.py
@@ -7,9 +7,7 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 
 class Solution:
     def is_anagram(self, s: str, t: str) -> bool:
-        list_s = list(s)
-        list_s.sort()
-        list_t = list(t)
-        list_t.sort()
-        return list_s == list_t
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+        return sorted_s == sorted_t
 
